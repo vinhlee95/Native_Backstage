3683ae3fb5fe877e246a641b5b6e169e676bd929
Vinhlee95 <lethanhvinh95@gmail.com>
2018-06-08 21:07:57 +0300

    add style

--- components/UI/Button.js
+++ components/UI/Button.js
@@ -3,14 +3,27 @@ import { View, Button } from 'react-native';
 
 const ReusableButton = (props) => {
    return(
-      <View>
+      <View style={buttonStyle}>
          <Button 
             title={props.title}
-            style={[{ backgroundColor: '#2b6edb', borderRadius: 5}, props.style]}
+            color="white"
             onPress={props.onPress}>
          </Button>
       </View>
    );
+}
+
+const buttonStyle = {
+   borderRadius: 5,
+   backgroundColor: '#2b6edb',
+   paddingTop: 5,
+   paddingBottom: 5,
+   marginTop: 10,
+   marginBottom: 10,
+   shadowColor: '#000',
+   shadowOffset: {width: 0, height: 3},
+   shadowOpacity: 0.2,
+   shadowRadius: 2,
 }
 
 export default ReusableButton;
