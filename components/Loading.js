11d5576e2b9db105ed602905c31d777eb203f008
Vinhlee95 <lethanhvinh95@gmail.com>
2018-06-20 19:26:08 +0300

    add image picker

--- components/Loading.js
+++ components/Loading.js
@@ -10,7 +10,7 @@ class Loading extends Component {
       firebase.auth().onAuthStateChanged((user) => {
          if (user) {
             this.setState({ isLogin: true });
-            this.props.navigation.navigate('Performer');
+            this.props.navigation.navigate('PerformerCreate');
          } else {
             this.setState({
                isLogin: false,
--- components/Performer/PerformerCreate.js
+++ components/Performer/PerformerCreate.js
@@ -1,18 +1,79 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView, Image, TouchableWithoutFeedback } from 'react-native';
+import { ImagePicker, Permissions } from 'expo';
+import { Ionicons } from '@expo/vector-icons';
+
 import Header from '../UI/Header';
+import Button from '../UI/Button';
+import ViewContainer from '../UI/View';
 
 class PerformerCreate extends Component {
-  render() {
-    return (
-      <View>
-         <Header
-            headerName="New performer"
-            notShowIcon headerRightTitle = "Done"
-            navigateBack={() => this.props.navigation.goBack()} />
-      </View>
-    )
-  }
+   state = { image: null };
+
+   pickImage = async () => {
+      const permissions = Permissions.CAMERA_ROLL;
+      const { status } = await Permissions.askAsync(permissions);
+
+      if(status === 'granted') {
+         let result = await ImagePicker.launchImageLibraryAsync({
+            allowsEditing: true,
+            aspect: [4, 3],
+         });
+
+         console.log(result);
+
+         if (!result.cancelled) {
+            this.setState({
+               image: result.uri
+            });
+         }
+      }
+
+      
+   }
+
+   render() {
+      return (
+         <View>
+            <Header
+               headerName="New performer"
+               notShowIcon headerRightTitle = "Done"
+               navigateBack={() => this.props.navigation.goBack()} />
+            <ScrollView>
+               <ViewContainer>
+                  {
+                     this.state.image
+                     ?
+                     <Image 
+                        source={{ uri: this.state.image }}
+                        style={styles.image} />
+                     :
+                     <TouchableWithoutFeedback onPress={() => this.pickImage()} >
+                        <View style={styles.image}>
+                           <Ionicons name="ios-camera" size={60} />
+                        </View>
+                     </TouchableWithoutFeedback>
+                  }
+                  
+               </ViewContainer>
+            </ScrollView>
+         </View>
+      )
+   }
+}
+
+const styles = {
+   image: {
+      backgroundColor: 'lightgrey',
+      width: 200,
+      height: 200,
+      borderRadius: 100,
+      marginTop: 15, marginBottom: 15,
+      marginLeft: 'auto', marginRight: 'auto',
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+   }
 }
 
 export default PerformerCreate;
