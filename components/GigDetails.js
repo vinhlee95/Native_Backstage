056ca2963a8e8e8fa56ac50bd5541a3dc79d97a3
Vinhlee95 <lethanhvinh95@gmail.com>
2018-07-21 23:09:16 +0300

    done with gig details

--- components/GigDetails.js
+++ components/GigDetails.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
-import { View, ScrollView } from 'react-native';
+import { View, Text, ScrollView, TouchableOpacity, Image } from 'react-native';
 
 import { HeaderTitle } from './UI/Header/index.js';
 import ListItem from './UI/ListItem.js';
+import Ionicons from '../node_modules/@expo/vector-icons/Ionicons';
+import Map from './Location/Map.js';
+
+const location = {
+   lat: 60.192059,
+   lng: 24.945831,
+   latDelta: 0.009,
+   lngDelta: 0.01
+}
 
 class GigDetails extends Component {
    static navigationOptions = ({ navigation }) => {
@@ -28,7 +37,9 @@ class GigDetails extends Component {
 
 
    render() {
-      console.log(this.state.gigDate)
+      console.log(this.state.product)
+      const { productImage, title } = this.state.product;
+      const { navigation } = this.props;
       return (
          <ScrollView>
             <ListItem
@@ -43,6 +54,60 @@ class GigDetails extends Component {
                rightTitle={this.state.gigTime}
                noArrow
             />
+
+            <TouchableOpacity
+               onPress={() => navigation.navigate('MapFullScreen', { location })}
+            >
+               <View style={styles.listItem}>
+                  <Ionicons name='ios-navigate-outline' size={25} />
+                  <View style={styles.content}>
+                     <View style={styles.leftCol}>
+                        <View style={styles.titleContainer}>
+                           <Text style={styles.title}>Location</Text>
+                           <Text style={styles.text}>{this.state.gigAddress}</Text>
+                        </View>
+                     </View>
+                     <View style={styles.rightCol}>
+                        <Map
+                           style={{ width: 100, height: 100, borderRadius: 5 }}
+                           location={location}
+                           
+                        />
+                        <Ionicons name='ios-arrow-forward' size={25}
+                           color='#e0e2e5'
+                           style={{marginLeft: 5}} />
+                     </View>
+                  </View>
+               </View>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+               onPress={() => navigation.navigate('PerformanceInfo', {
+                  performanceData: this.state.product
+               })}
+            >
+               <View style={styles.listItem}>
+                  <Ionicons name='ios-film-outline' size={25} />
+                  <View style={styles.content}>
+                     <View style={styles.leftCol}>
+                        <View style={styles.titleContainer}>
+                           <Text style={styles.title}>Performance</Text>
+                           <Text style={styles.text}>{title}</Text>
+                        </View>
+                     </View>
+                     <View style={styles.rightCol}>
+                        <Image
+                           source={{ uri: productImage }}
+                           style={{ width: 100, height: 100, borderRadius: 5 }}
+                        />
+                        <Ionicons name='ios-arrow-forward' size={25}
+                           color='#e0e2e5'
+                           style={{marginLeft: 5}} />
+                     </View>
+                  </View>
+               </View>
+            </TouchableOpacity>
+
             <ListItem
                icon='ios-person-outline'
                title='Customer Name'
@@ -57,6 +122,38 @@ class GigDetails extends Component {
          </ScrollView>
       )
    }
+}
+
+const styles = {
+   listItem: {
+      flexDirection: 'row',
+      backgroundColor: 'white',
+      paddingLeft: 10, paddingTop: 10,
+   },
+   content: {
+      flex: 1,
+      flexDirection: 'row',
+      borderBottomWidth: 1,
+      borderColor: '#e0e2e5',
+      paddingBottom: 10, paddingRight: 5,
+   },
+   leftCol: {
+      flexDirection: 'row', flex: 2,
+   },
+   titleContainer: {
+      width: '80%', marginLeft: 10
+   },
+   title: {
+      fontSize: 18,
+      marginBottom: 10,
+   },
+   text: { fontSize: 16 },
+   rightCol: {
+      flexDirection: 'row', flex: 1,
+      alignItems: 'center',
+
+   }
+
 }
 
 export default GigDetails;
--- components/Location/Map.js
+++ components/Location/Map.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { MapView } from 'expo';
 
+
 export default class Map extends React.Component {
    
-   
    render() {
-      const { lat, lng } = this.props.location;
+      const { location } = this.props;
       const initialRegion = {
-         latitude: lat ? lat : 60.192059,
-         longitude: lng ? lng : 24.945831,
-         latitudeDelta: 0.02,
-         longitudeDelta: 0.01,
+         latitude: location.lat ? location.lat : 60.192059,
+         longitude: location.lng ? location.lng : 24.945831,
+         latitudeDelta: location.latDelta ? location.latDelta : 0.02,
+         longitudeDelta: location.lngDelta ? location.lngDelta : 0.01,
       }
       return (
          <MapView
